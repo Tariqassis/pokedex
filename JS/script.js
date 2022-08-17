77e183797aa443787b9cc94d891442dd06aa1ca5
Tariq Assis <tariqassis@gmail.com>
2022-08-17 18:02:40 -0300

    update

--- JS/script.js
+++ JS/script.js
@@ -32,7 +32,7 @@ const renderPokemon = async (pokemon) => {
         pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']   ['front_default'];
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
         input.value = '';
         searchPokemon = data.id;
     } else {
